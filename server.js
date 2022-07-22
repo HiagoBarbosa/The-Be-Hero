aa548d45e08ac9178410b718ee8b0c823f895f28
HiagoBarbosa <hiaaugusto@hotmail.com>
2022-07-21 22:27:43 -0300

    a

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const server = jsonServer.create();
 const route = jsonServer('db.json');
 const middlewares = jsonServer.defaults();
 
-//const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
-app.listen(process.env.PORT || 8080, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+server.use(route);
+server.listen(port, ()=>{
+    console.log(`JSON SERVER IS RUNNING IN ${port}`)
 });
