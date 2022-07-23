74b3f82a09281ff185a15802d0dc00272231da26
HiagoBarbosa <hiaaugusto@hotmail.com>
2022-07-23 02:06:36 -0300

    foi retireado a conexão com o heroku pois estou fazendo separado

--- backend/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('json-server');
-const router = express.Router();
-
-const OngController = require("./controllers/ong");
-
-router.get("/ong", OngController.all);
-router.post("/ong", OngController.create);
-
-module.exports = router;
